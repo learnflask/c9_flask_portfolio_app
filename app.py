d2f259543a56b88711f0fad377678ee3c3c78753
Pythonic Man (Terrel Shumway) <gh3254@pythonic.ink>
2019-03-29 11:23:56 -0600

    adding numbers

--- app.py
+++ app.py
@@ -25,11 +25,8 @@ def add_numbers_post():
 	  if request.method == 'GET':
 	  	return render_template('add_numbers.html')
 	  elif request.method == 'POST':
-  	      print(request.form['text'].split())
-  	      total = 0
   	      try:
-  	      	for str_num in request.form['text'].split():
-  	      		total += int(str_num)
+              total = sum((int(x) for x in request.form['text'].split()))
   	      	return render_template('add_numbers.html', result=str(total))
   	      except ValueError:
   	      	return "Easy now! Let's keep it simple! 2 numbers with a space between them please"
